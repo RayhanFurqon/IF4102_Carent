5e50dd5db61d173f34928fd9cae4f006c61b87e1
Muhammad Yafie Setyo Wibowo <46883145+yafiesetyo@users.noreply.github.com>
2019-12-03 00:22:40 +0700

    Add files via upload

--- IF-41-02_PENYEWAAN KENDARAAN/routes/web.php
+++ IF-41-02_PENYEWAAN KENDARAAN/routes/web.php
@@ -11,6 +11,33 @@
 |
 */
 
-Route::get('/', 'PagesController@index');
-Route::get('/registration/customer', 'PagesController@regcus');
-Route::get('/registration/owner', 'PagesController@regown');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', 'HomeController@index');
+Route::get('/home/customer','homeCustController@index');
+Route::get('/home/owner','homeOwnerController@index');
+Route::get('/login','loginController@index');
+Route::get('/registrasi','registrasiController@index');
+Route::get('/registrasi/owner','registOwnerController@index');
+Route::get('/registrasi/customer','registCustController@index');
+Route::post('/registrasi/customer/submit','registCustController@submit');
+Route::post('/registrasi/owner/submit','registOwnerController@submit');
+Route::post('/login/auth','loginController@auth');
+Route::get('/logout','loginController@logout');
+Route::get('/customer/pesan/{id}','pesanController@getOwner');
+Route::post('/pesan/confirm/{id}','pesanController@pesan');
+Route::post('/bayar/confirm/{id}','bayarController@bayar');
+Route::get('/customer/list-pesanan','listPesanController@index');
+Route::post('/customer/lunas/{id}','listPesanController@pelunasan');
+Route::get('/customer/checkout','checkoutCustController@index');
+Route::get('/customer/profile','profileCustController@index');
+Route::post('/customer/changePass','profileCustController@validasi');
+Route::post('/customer/changePP','profileCustController@changeFoto');
+Route::get('/owner/garasi','garasiController@index');
+Route::post('/owner/changeStatus/{id}','garasiController@changestatus');
+Route::get('/owner/profile','profileOwnerController@index');
+Route::post('/owner/changePass','profileOwnerController@validasi');
+Route::post('/owner/changePP','profileOwnerController@changeFoto');
+Route::get('/owner/checkout','checkoutOwnerController@index');
